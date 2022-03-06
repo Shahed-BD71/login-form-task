e43ca395cbf447575a1bd291ce20ff6f46d200c4
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-03-06 18:16:24 +0600

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPass, setRepeatPass] = useState("");
+
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+    setRepeatPass("");
+  };
+
+  const handleSubmit = (event) => {
+    if (password === repeatPass && email) {
+      event.preventDefault();
+      alert("it works! check your dev tool");
+      console.log({ email, password });
+      clearInputs();
+    }
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    clearInputs();
+  };
+
   return (
     <section className="container">
-      <form className="form-area">
+      <form className="form-area" onSubmit={handleSubmit}>
         <div className="form-header">
           <h2>Sign Up</h2>
           <p>Please Fill up in this Form to creat an account</p>
           <hr />
         </div>
         <label>Email</label>
-        <input type="email" placeholder="Enter Email" utoFocus required></input>
-        <p className="errorMsg"></p>
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          autoFocus
+          required
+        ></input>
+        <p className="errorMsg">{}</p>
         <label>Password</label>
         <input
           type="password"
           placeholder="Enter Password"
           autoFocus
           required
+          value={password}
+          name="password"
+          onChange={(event) => setPassword(event.target.value)}
         ></input>
         <p className="errorMsg"></p>
         <label>Repeat Password</label>
         <input
-          type="confirm_password"
+          type="password"
           placeholder="Repeat Password"
           autoFocus
           required
+          value={repeatPass}
+          onChange={(event) => setRepeatPass(event.target.value)}
         ></input>
-        <p className="errorMsg"></p>
+        <p className="errorMsg">
+          {password !== repeatPass ? <p>Password not match</p> : null}
+        </p>
         <div className="form-footer">
           <p>
             By creating an account you agree to our{" "}
@@ -35,8 +74,12 @@ export default function App() {
           </p>
         </div>
         <div className="btn-container">
-          <button id="cancel">Cancel</button>
-          <button id="sign-up">Sign Up</button>
+          <button onClick={handleCancel} type="submit" id="cancel">
+            Cancel
+          </button>
+          <button type="submit" id="sign-up">
+            Sign Up
+          </button>
         </div>
       </form>
     </section>
