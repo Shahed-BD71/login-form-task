dc7e42ffb1235de648778ff8ff690c29e48b6db2
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-03-06 21:43:16 +0600

    updated to scss & install sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./App.scss";
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
